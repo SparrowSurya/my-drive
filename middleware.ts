3fb0f5803f677824038e3d8b591f9f47b8a2546d
sparrowsurya <92842745+SparrowSurya@users.noreply.github.com>
2025-07-03 11:55:16 +0530

    Added middleware for auth

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from "next/server";
+
+
+export default function middleware(request: NextRequest) {
+  const token = request.cookies.get("next-auth.session-token") || request.cookies.get("__Secure-next-auth.session-token");
+
+  if (!token) {
+    const searchParams = new URLSearchParams({
+      callbackUrl: request.nextUrl.pathname,
+    });
+    const loginUrl = `/auth/login?${searchParams}`;
+    return NextResponse.redirect(new URL(loginUrl, request.nextUrl.origin));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/drive/:path*",
+};
