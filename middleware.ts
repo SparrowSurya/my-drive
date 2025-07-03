0e99360976b6fe5bc0a1d15451fca5189c372558
sparrowsurya <92842745+SparrowSurya@users.noreply.github.com>
2025-07-03 13:21:43 +0530

    Redirect to /drive/home if user authenticated and visits /auth route

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,11 @@ import { NextResponse, NextRequest } from "next/server";
 export default function middleware(request: NextRequest) {
   const token = request.cookies.get("next-auth.session-token") || request.cookies.get("__Secure-next-auth.session-token");
 
-  if (!token) {
+  if (!!token && request.nextUrl.pathname.startsWith("/auth")) {
+    return NextResponse.redirect(new URL("/drive/home", request.nextUrl.origin));
+  }
+
+  if (!token && request.nextUrl.pathname.startsWith("/drive")) {
     const searchParams = new URLSearchParams({
       callbackUrl: request.nextUrl.pathname,
     });
@@ -15,6 +19,3 @@ export default function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: "/drive/:path*",
-};
