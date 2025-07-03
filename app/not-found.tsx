6593ad4c6fcecf1697c5df724e9795256d7ce289
sparrowsurya <92842745+SparrowSurya@users.noreply.github.com>
2025-07-03 11:05:01 +0530

    Added not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center mt-20 gap-3">
+      <div className="flex flex-col gap-3 w-[320px]">
+        <div className="flex flex-row items-center gap-3">
+          <Image
+            src="/assets/icons/logo.png"
+            alt="Google drive logo"
+            width="32"
+            height="32"
+          />
+          <div className="text-2xl">Drive</div>
+        </div>
+        <div className="">
+          <span className="font-bold mr-2">404</span>
+          Page not found
+        </div>
+        <div className="">The requested URL was not found on this server. That&apos;s all we know.</div>
+      </div>
+      <div>
+        <Image
+          src="/assets/images/robot.png"
+          alt="Google drive logo"
+          width="120"
+          height="120"
+          style={{ imageRendering: "pixelated" }}
+        />
+      </div>
+    </div>
+  );
+}
