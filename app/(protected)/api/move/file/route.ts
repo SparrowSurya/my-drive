517a883a78cc3bc8cfa1398b2b6f8111f605435f
sparrowsurya <92842745+SparrowSurya@users.noreply.github.com>
2026-05-23 18:00:31 +0530

    Added move file/folder routes

---
+++ app/(protected)/api/move/file/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { moveFile } from "@/lib/query/file";
+
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.json({ error: "something went wrong" }, { status: 401 });
+  }
+
+  const { email } = session.user;
+  const { fileId, folderId } = await req.json();
+
+  try {
+    await moveFile({ email }, fileId, folderId);
+    return NextResponse.json({ message: "File moved successfully" }, { status: 200 });
+  } catch {
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
